af5457827bd655f4fd0abf2d67ca02a502cb7e15
syahr <syahrezaraditya@gmail.com>
2022-03-16 19:06:22 +0100

    add useMovieFetch hook

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,36 +8,13 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 // components
 import Grid from './Grid';
 import Spinner from './Spinner';
+// get movie data with hook
+import { useMovieFetch } from './useMovieFetch';
 
 export default function Movie() {
     
-    const { movieId } = useParams
-    const [state, setState] = useState({});
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
-
-    console.log(state);
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setLoading(true);
-                setError(false);
-                const movie = await API.fetchMovie(movieId);
-                const credits = await API.fetchCredits(movieId);
-                const directors = credits.crew.filter(member => member.job === 'Director');
-                setState({
-                    ...movie,
-                    actors: credits.cast,
-                    directors
-                });
-                setLoading(false);
-            }
-            catch (error) {
-                setError(true);
-            }
-        }
-        fetchData()
-    }, [movieId])
+    const { movieId } = useParams();
+    const { state: movie, loading, error } = useMovieFetch(movieId);
 
     return (
         <>
